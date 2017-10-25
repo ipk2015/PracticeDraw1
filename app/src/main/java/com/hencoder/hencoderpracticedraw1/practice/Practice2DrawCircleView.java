a56b927a6adbb1e5e653c7ba6142fe97d70e78bd
1296064976@qq.com <201912@Gh>
2017-10-25 18:14:50 +0800

    finish p2

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,6 +2,8 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -26,5 +28,18 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+        int width = getWidth();
+        int height = getHeight();
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawCircle(width/4,height/4,height/5,paint);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(4);
+        canvas.drawCircle(width/4*3,height/4,height/5,paint);
+        paint.setStrokeWidth(50);
+        canvas.drawCircle(width/4*3,height/4*3,height/5,paint);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLUE);
+        canvas.drawCircle(width/4,height/4*3,height/5,paint);
     }
 }
